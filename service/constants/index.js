2358d616536bc974bb42cf4d0d22259d736a7e8e
Avinashtech-art <avinash.os@techversantinfotech.com>
2024-02-22 14:57:57 +0530

    latestcommit

--- service/constants/index.js
+++ service/constants/index.js
@@ -3,13 +3,20 @@ const AppRoutes = {
     GetUser : '/get-user',
     RegisterUser: '/register-user',
     DeleteUser: '/delete-user/:id',
+    Products:'/productslist',
+    ViewWishList:'/viewwishlist/:userId',
+    WishList:'/wishlist',
+    DeleteWishListItem:'/wishlist/:productId/:userId'
+    
 }
 const ClientRoutes = {
     Base: '/client',
     Login : '/login',
     Register: '/register',
     Home:'/home',
-    Order:'/order'
+    Order:'/order',
+    Cart:'/cart',
+    WishList:'/WishListPage'
 }
 
 module.exports = {ClientRoutes,AppRoutes};
--- service/utils/authMiddileware.js
+++ service/utils/authMiddileware.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 const jwtConfig = require('./jwtConfig');
 
-function authenticateToken(req, res, next) {
+const authenticateToken = (req, res, next) => {
   const token = req.header('Authorization');
   console.log('token', token)
 
   if (!token) {
-     res.status(401).json({ error: 'Unauthorized' });
-     res.send('/login')
+    res.status(401).json({ error: 'Unauthorized' });
+    res.send('/login')
   }
 
   jwt.verify(token, jwtConfig.secretKey, (err, user) => {
@@ -16,6 +16,8 @@ function authenticateToken(req, res, next) {
       return res.status(403).json({ error: 'Forbidden' });
     }
 
+    // here we are appending the user's information into the request payload, so that each endpoint can access it.
+    // it will be available in the request.user property of the actual function call.
     req.user = user;
     next();
   });
